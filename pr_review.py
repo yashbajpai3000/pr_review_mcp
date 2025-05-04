404e965b4da6f07407b9166e9efcc939b6f239c6
unarayan-sf <upendra.narayan@springfinancial.ca>
2025-05-04 17:16:53 +0530

    calc hmac sha

--- pr_review.py
+++ pr_review.py
@@ -725,4 +725,4 @@ def review_pr(owner, repo_name, pr_number): # Removed Celery decorator
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 6969)))
